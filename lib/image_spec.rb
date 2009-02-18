0aba931edfe28f12e8490cedabc4371a3fee02b2
Michael Sheakoski <michael@mjsdigital.com>
2009-02-18 06:36:38 -0500

    No need for filename/stream to be accessors or ivars

--- lib/image_spec.rb
+++ lib/image_spec.rb
@@ -3,24 +3,23 @@ Dir[File.join(File.dirname(__FILE__), 'parsers/*')].each { |f| require f }
 
 class ImageSpec
 
-  attr_reader :filename, :stream
   attr_reader :width, :height
 
   def initialize(file)
-    @stream = stream_for(file)
-    @filename = @stream.path || file
+    stream   = stream_for(file)
+    filename = stream.path || file
 
     @width, @height = case
-    when Parsers::GIF.gif?(@stream)
-      Parsers::GIF.dimensions(@stream)
-    when Parsers::JPEG.jpeg?(@stream)
-      Parsers::JPEG.dimensions(@stream)
-    when Parsers::PNG.png?(@stream)
-      Parsers::PNG.dimensions(@stream)
-    when Parsers::SWF.swf?(@stream)
-      Parsers::SWF.dimensions(@stream)
+    when Parsers::GIF.gif?(stream)
+      Parsers::GIF.dimensions(stream)
+    when Parsers::JPEG.jpeg?(stream)
+      Parsers::JPEG.dimensions(stream)
+    when Parsers::PNG.png?(stream)
+      Parsers::PNG.dimensions(stream)
+    when Parsers::SWF.swf?(stream)
+      Parsers::SWF.dimensions(stream)
     else
-      raise "#{File.basename(@filename)} is not supported. Sorry bub :("
+      raise "#{File.basename(filename)} is not supported. Sorry bub :("
     end
   end
 
