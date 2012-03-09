d1747691b5cc2d61dae1fd81b38294b182eaabaf
Brandon Anderson <anderson.brandon@gmail.com>
2012-03-08 18:04:12 -0600

    nbits fix
    Nbits determination was using 'c' for a signed integer, should've been
    'C' for unsigned int.

--- lib/parser/swf.rb
+++ lib/parser/swf.rb
@@ -27,7 +27,7 @@ class ImageSpec
         # Our 'signature' is the first 3 bytes
         # Either FWS or CWS.  CWS indicates compression
         signature = contents[0..2]
-
+        
         # Determine the length of the uncompressed stream
         length = contents[4..7].unpack('V').join.to_i
         
@@ -42,11 +42,11 @@ class ImageSpec
         end
 
         # Determine the nbits of our dimensions rectangle
-        nbits =contents.unpack('c'*contents.length)[8] >> 3
+        nbits = contents.unpack('C' * contents.length)[8] >> 3
 
         # Determine how many bits long this entire RECT structure is
         rectbits = 5 + nbits * 4    # 5 bits for nbits, as well as nbits * number of fields (4)
-
+        
         # Determine how many bytes rectbits composes (ceil(rectbits/8))
         rectbytes = (rectbits.to_f / 8).ceil
 
